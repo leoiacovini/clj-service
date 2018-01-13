0ca6a2a2f2326cc08f00160fc4a4be8c7fd54862
Leonardo Iacovini <leoiacovini@gmail.com>
2018-01-13 13:12:11 -0200

    Add token and crypto to system map

--- src/common_labsoft/system.clj
+++ src/common_labsoft/system.clj
@@ -3,7 +3,9 @@
             [common-labsoft.components.config :as components.config]
             [common-labsoft.components.pedestal :as components.pedestal]
             [common-labsoft.components.datomic :as components.datomic]
-            [common-labsoft.components.webapp :as components.webapp]))
+            [common-labsoft.components.webapp :as components.webapp]
+            [common-labsoft.components.token :as components.token]
+            [common-labsoft.components.crypto :as components.crypto]))
 
 (def system (atom nil))
 
@@ -12,7 +14,9 @@
     :config   (component/using (components.config/new-config config-name) [])
     :pedestal (component/using (components.pedestal/new-pedestal routes) [:config :webapp])
     :datomic  (component/using (components.datomic/new-datomic {}) [:config])
-    :webapp   (component/using (components.webapp/new-webapp) [:config :datomic])))
+    :token    (component/using (components.token/new-token) [:config])
+    :crypto   (component/using (components.crypto/new-crypto) [:config])
+    :webapp   (component/using (components.webapp/new-webapp) [:config :datomic :token :crypto])))
 
 (defn bootstrap! [config]
   (prn "Starting system!!")
