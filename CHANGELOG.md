a3b9735d8c5d8392011bcd3750951ef0bf9c6533
Leonardo Iacovini <leoiacovini@gmail.com>
2018-01-14 20:45:29 -0200

    Add schemas interceptors

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Change Log
 
+### [0.2.0-SNAPSHOT]
+- Add schema interceptors
+
 ### [0.1.2-SNAPSHOT]
 - Bug Fixes in Pedestal component
 
---
+++ src/common_labsoft/pedestal/interceptors/schema.clj
@@ -0,0 +1,16 @@
+(ns common-labsoft.pedestal.interceptors.schema
+  (:require [common-labsoft.schema :as schema]))
+
+(defn coerce [schema]
+  {:name  ::coerce
+   :enter (fn [context]
+            (if-let [body (-> context :request :json-params)]
+              (assoc-in context [:request :data] (schema/coerce body schema))
+              context))})
+
+(def coerce-output
+  {:name ::coerce-output
+   :leave (fn [context]
+            (if-let [schema (-> context :response :schema)]
+              (update-in context [:response :body] #(schema/coerce % schema))
+              context))})
