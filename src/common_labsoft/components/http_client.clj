00ffeb0a4ebe8d1f64670d02cff17724d0f61de3
Rafael Leal <rafaelealdias@gmail.com>
2018-01-21 15:35:03 -0200

    token retry logic

--- src/common_labsoft/components/http_client.clj
+++ src/common_labsoft/components/http_client.clj
@@ -2,29 +2,10 @@
   (:require [common-labsoft.protocols.config :as protocols.config]
             [common-labsoft.protocols.http-client :as protocols.http-client]
             [common-labsoft.protocols.token :as protocols.token]
-            [clj-http.client :as client]))
+            [clj-http.client :as client]
+            [cheshire.core :as cheshire]))
 
-(defn resolve-token
-  "Receives a token. If its valid, returns it. If its not, generate a valid token and return it."
-  [token]
-  (let [token (:token @token)
-        record (:record @token)
-        content (:content @token)]
-    (if (protocols.token/verify record token)
-      token
-      (protocols.token/encode record content))))
 
-(defrecord HttpClient
-  [config token] ;"This token is a atom with keys :record :content :token"
-  protocols.http-client/HttpClient
-  (get-hosts [this] (protocols.config/get! config :services))
-  (raw-req! [this data] (client/request data))
-  (authd-req! [this data]
-    (let [valid-token (resolve-token token)]
-      (if (not= valid-token token)
-        (swap! token (fn [t] (merge @t {:token valid-token}))))
-      (protocols.http-client/raw-req! this (merge data {:headers {:authorization (str "Bearer " (:token @token))}}))))
-  )
 
 (defn- resolve-replace-map
   [template-str replace-map]
@@ -39,9 +20,62 @@
         endpoints (get service :endpoints)]
     (resolve-replace-map (str (get service :host) (get endpoints endpoint)) replace-map)))
   ([hosts service-host endpoint]
-    (render-route hosts service-host endpoint {}))
-  )
+    (render-route hosts service-host endpoint {})))
 
+(defn render-keyworded-url
+  "Resolves :customers/one-customer to its url"
+  [hosts key replace-map]
+  (let [service-host (keyword (namespace key))
+        endpoint (keyword (name key))]
+    (render-route hosts service-host endpoint replace-map)))
+
+(defn resolve-token
+  "Receives a token. If its valid, returns it. If its not, generate a valid token and return it."
+  [token-component token service-name service-password]
+  (let [auth-payload {:auth/service-name service-name :auth/auth-password service-password}
+        config (:config token-component)
+        services (protocols.config/get! config :services)]
+    (if (protocols.token/verify token-component token)
+      token
+      (-> (render-route services :auth :service-token)
+          (client/post {:form-params auth-payload :content-type :json})
+          (:body)
+          (cheshire/parse-string true)
+          (:token)))))
+
+;; Transform methods: receives and returns request's data.
+(defn transform-url
+  "Generates a url if the data was provided with host/endpoint or keyworded url. Applies replace-map"
+  [data hosts]
+  (let [service (:host data)
+        endpoint (:endpoint data)
+        replace-map (:replace-map data {})
+        url (:url data)]
+    (if (keyword? url)
+      (let [url (render-keyworded-url hosts url (:replace-map data {}))]
+        (merge (dissoc data :replace-map) {:url url}))
+      (if (and (some? service) (some? endpoint))
+        (merge (dissoc data :replace-map :endpoint :host) {:url (render-route hosts service endpoint replace-map)})
+        data))))
+
+(defn- transform-method
+  "Transforms request's method"
+  [data method]
+  (merge data {:method method}))
+
+(defrecord HttpClient
+  [config token] ;"This token is a atom with keys :record :content :token"
+  protocols.http-client/HttpClient
+  (get-hosts [this] (protocols.config/get! config :services))
+  (raw-req! [this data] (client/request data))
+  (authd-req! [this data]
+    (let [service-name (protocols.config/get! config :service-name)
+          service-password (protocols.config/get! config :service-password)
+          valid-token (resolve-token this token service-name service-password)]
+      (if (not= valid-token token)
+        (swap! token (fn [t] (merge @t {:token valid-token}))))
+      (protocols.http-client/raw-req! this (merge {:headers {:authorization (str "Bearer " (:token @token))}} data))))
+  )
 
 (defn new-http-client [config token]
   (map->HttpClient {:config config :token token}))
--- test/common_labsoft/http_client_test.clj
+++ test/common_labsoft/http_client_test.clj
@@ -6,7 +6,7 @@
             [common-labsoft.components.config :as config]
             [common-labsoft.components.token :as token]))
 
-(def test-config {:customers {:host "https://customers.labsoft.host"
+(def test-hosts {:customers {:host       "https://customers.labsoft.host"
                               :endpoints { :one-customer "/customers/:id"
                                            :all-customers "/customers" }}
                   })
@@ -16,14 +16,23 @@
 (def http (new-http-client configr (atom {})))              ;; TODO: fix
 
 (fact "get-hosts must returns map with services hosts and endpoints"
-      (get-hosts http) => test-config)
+      (get-hosts http) => test-hosts)
 ;
 (fact "when rendering routes"
       (fact "create a full url even without replace-map"
-            (render-route test-config :customers :all-customers) => "https://customers.labsoft.host/customers")
+            (render-route test-hosts :customers :all-customers) => "https://customers.labsoft.host/customers")
       (fact "create a url based on replace-map"
-            (render-route test-config :customers :one-customer {:id 42}) => "https://customers.labsoft.host/customers/42"))
+            (render-route test-hosts :customers :one-customer {:id 42}) => "https://customers.labsoft.host/customers/42")
+      (fact "create a full url from namespaced keywords"
+            (render-keyworded-url test-hosts :customers/one-customer {:id 42}) => "https://customers.labsoft.host/customers/42"))
 
 (fact "when performing a request"
       (fact "must be able to do a raw request"
             (:status (raw-req! http {:method :get :url "https://google.com"})) => 200))
+
+(fact "data must be transformed"
+      (fact "to generate the url with namespaced keyword url"
+            (transform-url {:url :customers/one-customer :replace-map {:id 42}} test-hosts) => {:url "https://customers.labsoft.host/customers/42"})
+      (fact "to generate the url with host and endpoint"
+            (transform-url {:host :customers :endpoint :one-customer :replace-map {:id 42}} test-hosts) => {:url "https://customers.labsoft.host/customers/42"}))
+
