e624a416470cf0d620232c2b1b8edfee4e2793e7
Rafael Leal <rafaelealdias@gmail.com>
2018-01-22 23:50:00 -0200

    Response and requests coerce, small fixes

--- src/common_labsoft/components/http_client.clj
+++ src/common_labsoft/components/http_client.clj
@@ -2,6 +2,7 @@
   (:require [common-labsoft.protocols.config :as protocols.config]
             [common-labsoft.protocols.http-client :as protocols.http-client]
             [common-labsoft.protocols.token :as protocols.token]
+            [common-labsoft.adapt :as adapt]
             [clj-http.client :as client]
             [cheshire.core :as cheshire]))
 
@@ -28,36 +29,44 @@
 (defn resolve-token
   "Receives a token. If its valid, returns it. If its not, generate a valid token and return it."
   [token-component token service-name service-password]
-  (let [auth-payload {:auth/service-name service-name :auth/auth-password service-password}
+  (let [auth-payload {:auth/service service-name :auth/password service-password}
         config (:config token-component)
         services (protocols.config/get! config :services)]
     (if (protocols.token/verify token-component token)
       token
       (-> (render-route services :auth :service-token)
           (client/post {:form-params auth-payload :content-type :json})
-          (:body)
+          :body
           (cheshire/parse-string true)
-          (:token/jwt)))))
+          :token/jwt))))
 
 ;; Transform methods: receives and returns request's data.
 (defn transform-url
   "Generates a url if the data was provided with host/endpoint or keyworded url. Applies replace-map"
-  [data hosts]
-  (let [service (:host data)
-        endpoint (:endpoint data)
-        replace-map (:replace-map data {})
-        url (:url data)]
-    (if (keyword? url)
-      (let [url (render-keyworded-url hosts url (:replace-map data {}))]
-        (merge (dissoc data :replace-map) {:url url}))
-      (if (and (some? service) (some? endpoint))
-        (merge (dissoc data :replace-map :endpoint :host) {:url (render-route hosts service endpoint replace-map)})
-        data))))
+  [{:keys [host endpoint replace-map url] :as data} hosts]
+  (if (keyword? url)
+    (let [url (render-keyworded-url hosts url (or replace-map {}))]
+      (merge (dissoc data :replace-map) {:url url}))
+    (if (and (some? host) (some? endpoint))
+      (merge (dissoc data :replace-map :endpoint :host) {:url (render-route hosts host endpoint (or replace-map {}))})
+      data)))
 
 (defn transform-method
   "Transforms request's method"
-  [data method]
-  (merge data {:method method}))
+  [method data]
+  (assoc data :method method))
+
+(defn internalize-response
+  [req resp]
+  (let [json-or-edn (if (= (get-in resp [:headers :content-type]) :application/json)
+                      :json
+                      :edn)]
+    (adapt/internalize json-or-edn (:body resp) (:schema-resp req)))
+  )
+
+(defn externalize-request
+  [{:keys [content-type body schema-req] :as req}]
+  (assoc req :body (adapt/externalize content-type body schema-req)))
 
 (defrecord HttpClient
   [config token]
@@ -70,14 +79,30 @@
           valid-token (resolve-token this token service-name service-password)]
       (if (not= valid-token @token)
         (swap! token (fn [t] (atom valid-token))))
-      (protocols.http-client/raw-req! this (transform (merge {:headers {:authorization @token}} data)))))
-  (authd-req! [this data] (protocols.http-client/authd-req! this data identity))
-  (authd-get! [this data] (protocols.http-client/authd-req! this data (fn [req] (transform-method req :get))))
-  (authd-post! [this data] (protocols.http-client/authd-req! this data (fn [req] (transform-method req :post))))
-  (authd-patch! [this data] (protocols.http-client/authd-req! this data (fn [req] (transform-method req :patch))))
-  (authd-delete! [this data] (protocols.http-client/authd-req! this data (fn [req] (transform-method req :delete))))
-  (authd-put! [this data] (protocols.http-client/authd-req! this data (fn [req] (transform-method req :put))))
-  )
+      (let [req-defaults {:headers {:authorization @token} :content-type :json}
+            req-data (merge req-defaults data)
+            trans-req-data (externalize-request (transform req-data))
+            raw-resp (protocols.http-client/raw-req! this trans-req-data)
+            resp (internalize-response trans-req-data raw-resp)
+            ] resp)))
+  (authd-req! [this data]
+    (protocols.http-client/authd-req! this data identity))
+  (authd-get! [this data]
+    (protocols.http-client/authd-req! this data (partial transform-method :get)))
+  (authd-post! [this data]
+    (protocols.http-client/authd-req! this data (partial transform-method :post)))
+  (authd-patch! [this data]
+    (protocols.http-client/authd-req! this data (partial transform-method :patch)))
+  (authd-delete! [this data]
+    (protocols.http-client/authd-req! this data (partial transform-method :delete)))
+  (authd-put! [this data]
+    (protocols.http-client/authd-req! this data (partial transform-method :put))))
 
-(defn new-http-client [config token]
-  (map->HttpClient {:config config :token token}))
+
+(defn new-http-client
+  ([config token]
+    (map->HttpClient {:config config :token token}))
+  ([token]
+   (map->HttpClient {:token token}))
+  ([]
+   (map->HttpClient {:token (atom "invalid-token")})))
