94be669adf5bd89ba1a5ac92654b7539e21b085b
Leonardo Iacovini <leoiacovini@gmail.com>
2018-06-15 23:04:08 -0300

    mend

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject leoiacovini/clj-service "1.1.2"
+(defproject leoiacovini/clj-service "1.1.3"
   :description "Common code for creating Clojure services using pedestal"
   :license {:name         "MIT"
             :distribution :repo}
