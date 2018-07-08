8403435b662206956d4d0a321e816a7604555613
Leonardo Iacovini <leoiacovini@gmail.com>
2018-07-08 01:33:28 -0300

    Stop signing releases

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :description "Common code for creating Clojure services using pedestal"
   :license {:name         "MIT"
             :distribution :repo}
+  :deploy-repositories [["releases" {:sign-releases false :url "https://clojars.org"}]
+                        ["snapshots" {:sign-releases false :url "https://clojars.org"}]]
   :url "https://github.com.br/leoiacovini/clj-service"
   :plugins [[lein-midje "3.2.1"]]
   :dependencies [[org.clojure/clojure "1.9.0"]
